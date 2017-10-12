7046a1642ce2ab021b6b79be8745662d1da48d78
johnD20 <32309794+johnD20@users.noreply.github.com>
2017-10-12 13:12:26 -0600

    tits

--- heeloc/heeloc/Program.cs
+++ heeloc/heeloc/Program.cs
@@ -1,12 +1,1 @@
-﻿using System;
-
-namespace heeloc
-{
-    class MainClass
-    {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");;
-        }
-    }
-}
+﻿
